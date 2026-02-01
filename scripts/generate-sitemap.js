430e635a100c59f004fbd88dd769ffb161076619
Tarekul <tarekul03.muslim@gmail.com>
2026-02-01 12:39:22 +0530

    edit

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const BASE_URL = 'https://medix.com'; // TODO: Get from env
+const BASE_URL = 'https://medix-peach.vercel.app';
 
 const routes = [
     '/',
